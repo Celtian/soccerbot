f7676f08f20ff966ea800a40db973c1d0cd831ff
Dominik Hladík <dominik.hladik@seznam.cz>
2021-02-28 11:03:34 +0100

    fix(package): missing package.json added into dist

--- scripts/copy-dist.ts
+++ scripts/copy-dist.ts
@@ -1,8 +1,8 @@
 import { copyFileSync } from 'fs';
 
-console.log('Copying README.ms & LICENSE into dist.');
+console.log('Copying README.ms & LICENSE && package.json into dist.');
 
-const copyFiles = ['README.md', 'LICENSE'];
+const copyFiles = ['README.md', 'LICENSE', 'package.json'];
 for (const file of copyFiles) {
   copyFileSync(`./${file}`, `./dist/${file}`);
 }
