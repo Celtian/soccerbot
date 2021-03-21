0020ac61f1975c6c540bbe4aee8405275dc0b253
Dominik Hladík <dominik.hladik@seznam.cz>
2021-03-21 15:47:31 +0100

    chore(user-agent): user agent randomized

--- lib/clients/shared/index.ts
+++ lib/clients/shared/index.ts
@@ -2,6 +2,13 @@ import fetch from 'cross-fetch';
 import jsdom from 'jsdom';
 import { SoccerBotPlayer, SoccerBotResponse, SoccerBotTeam } from '../../shared';
 
+const USER_AGENTS: string[] = [
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+  'Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1',
+  'Mozilla/5.0 (Linux; Android 10; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36',
+  'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36'
+];
+
 export abstract class SoccerBotClient {
   public abstract league(id: string, season?: string): Promise<SoccerBotResponse<SoccerBotTeam[]>>;
 
@@ -9,7 +16,10 @@ export abstract class SoccerBotClient {
 
   protected async fetchPage(url: string): Promise<string> {
     try {
-      const response = await fetch(url);
+      const headers = {
+        'User-Agent': USER_AGENTS[Math.floor(Math.random() * USER_AGENTS.length)]
+      };
+      const response = await fetch(url, { headers });
       if (response.status >= 400 && response.status < 600) {
         throw new Error(`Bad response ${response.status} for url: ${url}`);
       }
@@ -26,9 +36,7 @@ export abstract class SoccerBotClient {
   }
 
   protected selectArray(virtualNode: Document, selector: string): HTMLTableRowElement[] {
-    return [].slice.call(
-      virtualNode.querySelectorAll(selector)
-    )
+    return [].slice.call(virtualNode.querySelectorAll(selector));
   }
 
   protected getTextAndTrim(element: Element): string {
