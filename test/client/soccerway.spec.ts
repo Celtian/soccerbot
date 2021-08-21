66273421d13ffb6e9414c3b53296829969e61c28
Dominik Hladík <dominik.hladik@seznam.cz>
2021-08-21 12:55:10 +0200

    chore(deps): jest updated

--- test/client/soccerway.spec.ts
+++ test/client/soccerway.spec.ts
@@ -10,11 +10,12 @@ describe('SoccerBotSoccerwayClient', () => {
     client = new SoccerBotSoccerwayClient(5);
   });
 
-  describe('leagueUrl',() => {
+  describe('leagueUrl', () => {
     it('should return correct value', () => {
-      expect(client.leagueUrl('r59164','20202021'))
-       .toEqual('https://int.soccerway.com/national/country-slug/team-slug/20202021/regular-season/r59164/tables/');
-    })
+      expect(client.leagueUrl('r59164', '20202021')).toEqual(
+        'https://int.soccerway.com/national/country-slug/team-slug/20202021/regular-season/r59164/tables/'
+      );
+    });
 
     it('should return undefined', () => {
       expect(client.leagueUrl(undefined, '20202021')).toEqual(undefined);
@@ -28,82 +29,82 @@ describe('SoccerBotSoccerwayClient', () => {
       expect(client.leagueUrl('', '20202021')).toEqual(undefined);
       expect(client.leagueUrl('r59164', '')).toEqual(undefined);
       expect(client.leagueUrl('', '')).toEqual(undefined);
-    })
-  })
+    });
+  });
 
-  describe('teamUrl',() => {
+  describe('teamUrl', () => {
     it('should return correct value', () => {
       expect(client.teamUrl('533')).toEqual('https://int.soccerway.com/teams/country-slug/team-slug/533/squad/');
-    })
+    });
 
     it('should return undefined', () => {
       expect(client.teamUrl(undefined)).toEqual(undefined);
       expect(client.teamUrl(null)).toEqual(undefined);
       expect(client.teamUrl('')).toEqual(undefined);
-    })
-  })
+    });
+  });
 
-  describe('playerUrl',() => {
+  describe('playerUrl', () => {
     it('should return correct value', () => {
       expect(client.playerUrl('193498')).toEqual('https://int.soccerway.com/players/player-slug/193498/');
-    })
+    });
 
     it('should return undefined', () => {
       expect(client.playerUrl(undefined)).toEqual(undefined);
       expect(client.playerUrl(null)).toEqual(undefined);
       expect(client.playerUrl('')).toEqual(undefined);
-    })
-  })
+    });
+  });
 
-  describe('league',() => {
+  describe('league', () => {
     beforeEach(() => {
-      const handleSpy = spyOn(SoccerBotSoccerwayClient.prototype as any, 'fetchPage');
-      handleSpy.and.callFake(() => {
+      const handleSpy = jest.spyOn(SoccerBotSoccerwayClient.prototype as any, 'fetchPage');
+      handleSpy.mockImplementation(() => {
         return new Promise((resolve) => {
           resolve(LEAGUE_HTML);
         });
       });
-    })
+    });
 
-    it('should return league', async() => {
+    it('should return league', async () => {
       expect(await client.league('r59164', '20202021')).toEqual(LEAGUE_DATA);
-    })
-  })
+    });
+  });
 
-  describe('team',() => {
+  describe('team', () => {
     beforeEach(() => {
-      const handleSpy = spyOn(SoccerBotSoccerwayClient.prototype as any, 'fetchPage');
-      handleSpy.and.callFake(() => {
+      const handleSpy = jest.spyOn(SoccerBotSoccerwayClient.prototype as any, 'fetchPage');
+      handleSpy.mockImplementation(() => {
         return new Promise((resolve) => {
           resolve(TEAM_HTML);
         });
       });
 
-      const handlePlayerSpy = spyOn(SoccerBotSoccerwayClient.prototype as any, 'player');
-      handlePlayerSpy.and.callFake(() => {
+      const handlePlayerSpy = jest.spyOn(SoccerBotSoccerwayClient.prototype as any, 'player');
+      handlePlayerSpy.mockImplementation(() => {
         return new Promise((resolve) => {
           resolve(PLAYER_DATA);
         });
       });
-    })
+    });
 
-    it('should return team', async() => {
+    it('should return team', async () => {
       expect(await client.team('533')).toEqual(TEAM_DATA);
-    })
-  })
+    });
+  });
 
-  describe('player',() => {
+  describe('player', () => {
     beforeEach(() => {
-      const handleSpy = spyOn(SoccerBotSoccerwayClient.prototype as any, 'fetchPage');
-      handleSpy.and.callFake(() => {
+      const handleSpy = jest.spyOn(SoccerBotSoccerwayClient.prototype as any, 'fetchPage');
+      handleSpy.mockImplementation(() => {
         return new Promise((resolve) => {
           resolve(PLAYER_HTML);
         });
       });
-    })
+    });
 
-    it('should return player', async() => {
+    it('should return player', async () => {
       expect(await client.player('193498')).toEqual(PLAYER_DATA);
-    })
-  })
-})
+    });
+  });
+});
--- test/client/transfermarkt.spec.ts
+++ test/client/transfermarkt.spec.ts
@@ -8,13 +8,13 @@ describe('SoccerBotTransfermarktClient', () => {
     client = new SoccerBotTransfermarktClient();
   });
 
-  describe('leagueUrl',() => {
+  describe('leagueUrl', () => {
     it('should return correct value', () => {
-      expect(client.leagueUrl('TS1'))
-       .toEqual('https://www.transfermarkt.com/slug/startseite/wettbewerb/TS1');
-      expect(client.leagueUrl('TS1','2018'))
-       .toEqual('https://www.transfermarkt.com/slug/startseite/wettbewerb/TS1/plus?saison_id=2018');
-    })
+      expect(client.leagueUrl('TS1')).toEqual('https://www.transfermarkt.com/slug/startseite/wettbewerb/TS1');
+      expect(client.leagueUrl('TS1', '2018')).toEqual(
+        'https://www.transfermarkt.com/slug/startseite/wettbewerb/TS1/plus?saison_id=2018'
+      );
+    });
 
     it('should return undefined', () => {
       expect(client.leagueUrl(undefined, '2018')).toEqual(undefined);
@@ -25,48 +25,48 @@ describe('SoccerBotTransfermarktClient', () => {
 
       expect(client.leagueUrl('', '2018')).toEqual(undefined);
       expect(client.leagueUrl('', '')).toEqual(undefined);
-    })
-  })
+    });
+  });
 
-  describe('teamUrl',() => {
+  describe('teamUrl', () => {
     it('should return correct value', () => {
       expect(client.teamUrl('533')).toEqual('https://www.transfermarkt.com/slug/kader/verein/533/plus/1');
-    })
+    });
 
     it('should return undefined', () => {
       expect(client.teamUrl(undefined)).toEqual(undefined);
       expect(client.teamUrl(null)).toEqual(undefined);
       expect(client.teamUrl('')).toEqual(undefined);
-    })
-  })
+    });
+  });
 
-  describe('league',() => {
+  describe('league', () => {
     beforeEach(() => {
-      const handleSpy = spyOn(SoccerBotTransfermarktClient.prototype as any, 'fetchPage');
-      handleSpy.and.callFake(() => {
+      const handleSpy = jest.spyOn(SoccerBotTransfermarktClient.prototype as any, 'fetchPage');
+      handleSpy.mockImplementation(() => {
         return new Promise((resolve) => {
           resolve(LEAGUE_HTML);
         });
       });
-    })
+    });
 
-    it('should return league', async() => {
+    it('should return league', async () => {
       expect(await client.league('TS1')).toEqual(LEAGUE_DATA);
-    })
-  })
+    });
+  });
 
-  describe('team',() => {
+  describe('team', () => {
     beforeEach(() => {
-      const handleSpy = spyOn(SoccerBotTransfermarktClient.prototype as any, 'fetchPage');
-      handleSpy.and.callFake(() => {
+      const handleSpy = jest.spyOn(SoccerBotTransfermarktClient.prototype as any, 'fetchPage');
+      handleSpy.mockImplementation(() => {
         return new Promise((resolve) => {
           resolve(TEAM_HTML);
         });
       });
-    })
+    });
 
-    it('should return team', async() => {
+    it('should return team', async () => {
       expect(await client.team('62')).toEqual(TEAM_DATA);
-    })
-  })
-})
+    });
+  });
+});
