95f347b7c07ace69b035b7dea50eb84c16d6bafa
Dominik Hladík <dominik.hladik@coex.cz>
2022-12-28 21:34:29 +0100

    feat(es6): es6 build added

---
+++ scripts/prepare-dist.ts
@@ -0,0 +1,15 @@
+import { writeFileSync } from 'fs-extra';
+import { join } from 'path';
+import packageJson from '../dist/package.json';
+
+// Modify package.json in dist folder
+const pkg: Record<string, any> = packageJson;
+
+pkg.scripts = {};
+pkg.devDependencies = {};
+pkg.main = 'es5/index.js';
+pkg.module = 'es6/index.js';
+pkg.sideEffects = false;
+
+writeFileSync(join(__dirname, '..', 'dist', 'package.json'), JSON.stringify(pkg, null, 2));
+console.log('File package.json modified:', pkg);
